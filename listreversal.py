312133fef58afeda163119a42110671662718a24
vmnayak-pract <72183643+vmnayak-pract@users.noreply.github.com>
2020-10-01 11:05:12 +0530

    Update listreversal.py
    Added input from user

--- listreversal.py
+++ listreversal.py
@@ -1,9 +1,17 @@
-s = ["a", "b", "c", "d"]
+n = int(input("Enter the number of items in List: "))
+s = []
+
+for i in range(n):
+   lst = input("Enter list item value: ")
+   s.append(lst)
+
 print("The list is ", s)
+
 i = 0
 while i < len(s)/2:
    t = s[len(s)-1-i]
    s[len(s)-1-i] = s[i]
    s[i] = t
    i += 1
+   
 print("The reversed list is ", s)
